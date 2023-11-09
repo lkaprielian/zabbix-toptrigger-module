7087730f625e7bc83125fb3dc48c3e713995ced4
lkaprielian <lisakaprielian.lk@gmail.com>
2023-11-09 14:49:55 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,22 +1,18 @@
-<?php declare(strict_types=1);
-
+<?php declare(strict_types = 1);
+ 
 namespace Modules\TopTriggers;
-
+ 
 use APP;
-
-class Module extends \Zabbix\Core\CModule
-{
-	public function init(): void
-	{
+ 
+class Module extends \Zabbix\Core\CModule {
+	public function init(): void {
 		// Initialize main menu (CMenu class instance).
 		APP::Component()->get('menu.main')
 			->findOrAdd(_('Reports'))
-			->getSubmenu()
-			->insertAfter(
-				'Availability report',
-				(new \CMenuItem(_('Top triggers')))
-					->setAction('toptriggers.view')
-			);
+				->getSubmenu()
+					->insertAfter('Availability report', (new \CMenuItem(_('Top triggers')))
+						->setAction('userprofile.edit')
+					);
 	}
 }
 ?>
