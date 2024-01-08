cd66f80f357596893ac7ac29cb0851bde66284d8
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-08 11:52:44 +0100

    Update CControllerBGAvailReport.php

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -122,8 +122,8 @@ abstract class CControllerBGAvailReport extends CController {
 			$triggersEventCount[$row['objectid']] = $row['cnt_event'];
 		}
 		
-		print($triggersEventCount);	
-		
+		print_r($triggersEventCount);	
+
 		if ($filter['only_with_problems']) {
 			// Find all triggers that went into PROBLEM state
 			// at any time in given time frame
