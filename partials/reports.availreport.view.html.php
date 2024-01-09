31b943569088d4981a6f37e19ae9d76adfa12d9c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-09 17:51:13 +0100

    Update reports.availreport.view.html.php

--- partials/reports.availreport.view.html.php
+++ partials/reports.availreport.view.html.php
@@ -57,9 +57,10 @@ foreach ($triggers as $trigger) {
 		$allowed_ui_problems
 			? new CLink($trigger['description'],
 				(new CUrl('zabbix.php'))
-					->setArgument('action', 'problem.view')
-					->setArgument('filter_name', '')
-					->setArgument('triggerids', [$trigger['triggerid']])
+					// ->setArgument('action', 'problem.view')
+					// ->setArgument('filter_name', '')
+					// ->setArgument('triggerids', [$trigger['triggerid']])
+					->setMenuPopup(CMenuPopupHelper::getTrigger($trigger['triggerid'], 0))
 			)
 			: $trigger['description'],
 
