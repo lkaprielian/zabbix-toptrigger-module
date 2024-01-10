b975a95325fbe44b09003ad548c587232a870705
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-10 18:28:01 +0100

    Update reports.availreport.view.html.php

--- partials/reports.availreport.view.html.php
+++ partials/reports.availreport.view.html.php
@@ -6,7 +6,7 @@ $table = (new CTableInfo());
 
 $view_url = $data['view_curl']->getUrl();
 
-print_r($data['filter']['sort']);
+// print_r($data['filter']['sort']);
 
 $table->setHeader([
 	(new CColHeader(_('Host'))),
