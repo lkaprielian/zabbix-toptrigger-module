88f5f1bcba0edb412601af4f2de50c0f7c579a1a
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-10 11:16:32 +0100

    Update reports.availreport.filter.php

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -1,47 +1,47 @@
 <?php declare(strict_types = 1);
 
 $filter_column = (new CFormList())
-	// ->addRow((new CLabel(_('Template groups'), 'tpl_groupids_#{uniqid}_ms')),
-	// 	(new CMultiSelect([
-	// 		'name' => 'tpl_groupids[]',
-	// 		'object_name' => 'hostGroup',
-	// 		'data' => array_key_exists('tpl_groups_multiselect', $data) ? $data['tpl_groups_multiselect'] : [],
-	// 		'popup' => [
-	// 			'parameters' => [
-	// 				'srctbl' => 'template_groups',
-	// 				'srcfld1' => 'groupid',
-	// 				'dstfrm' => 'zbx_filter',
-	// 				'dstfld1' => 'tpl_groupids_',
-	// 				'with_templates' => true,
-	// 				'editable' => true,
-	// 				'enrich_parent_groups' => true
-	// 			]
-	// 		]
-	// 	]))
-	// 		->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
-	// 		->setId('tpl_groupids_#{uniqid}')
-	// )
-	// ->addRow((new CLabel(_('Templates'), 'templateids_#{uniqid}_ms')),
-	// 	(new CMultiSelect([
-	// 		'name' => 'templateids[]',
-	// 		'object_name' => 'templates',
-	// 		'data' => array_key_exists('templates_multiselect', $data) ? $data['templates_multiselect'] : [],
-	// 		'popup' => [
-	// 			'filter_preselect' => [
-	// 				'id' => 'tpl_groupids_',
-	// 				'submit_as' => 'templategroupid'
-	// 			],
-	// 			'parameters' => [
-	// 				'srctbl' => 'templates',
-	// 				'srcfld1' => 'hostid',
-	// 				'dstfrm' => 'zbx_filter',
-	// 				'dstfld1' => 'templateids_'
-	// 			]
-	// 		]
-	// 	]))
-	// 		->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
-	// 		->setId('templateids_#{uniqid}')
-	// )
+	->addRow((new CLabel(_('Template groups'), 'tpl_groupids_#{uniqid}_ms')),
+		(new CMultiSelect([
+			'name' => 'tpl_groupids[]',
+			'object_name' => 'hostGroup',
+			'data' => array_key_exists('tpl_groups_multiselect', $data) ? $data['tpl_groups_multiselect'] : [],
+			'popup' => [
+				'parameters' => [
+					'srctbl' => 'template_groups',
+					'srcfld1' => 'groupid',
+					'dstfrm' => 'zbx_filter',
+					'dstfld1' => 'tpl_groupids_',
+					'with_templates' => true,
+					'editable' => true,
+					'enrich_parent_groups' => true
+				]
+			]
+		]))
+			->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
+			->setId('tpl_groupids_#{uniqid}')
+	)
+	->addRow((new CLabel(_('Templates'), 'templateids_#{uniqid}_ms')),
+		(new CMultiSelect([
+			'name' => 'templateids[]',
+			'object_name' => 'templates',
+			'data' => array_key_exists('templates_multiselect', $data) ? $data['templates_multiselect'] : [],
+			'popup' => [
+				'filter_preselect' => [
+					'id' => 'tpl_groupids_',
+					'submit_as' => 'templategroupid'
+				],
+				'parameters' => [
+					'srctbl' => 'templates',
+					'srcfld1' => 'hostid',
+					'dstfrm' => 'zbx_filter',
+					'dstfld1' => 'templateids_'
+				]
+			]
+		]))
+			->setWidth(ZBX_TEXTAREA_FILTER_STANDARD_WIDTH)
+			->setId('templateids_#{uniqid}')
+	)
 	->addRow((new CLabel(_('Template trigger'), 'tpl_triggerids_#{uniqid}_ms')),
 		(new CMultiSelect([
 			'name' => 'tpl_triggerids[]',
@@ -151,50 +151,50 @@ if (array_key_exists('render_html', $data)) {
 		$('[name="filter_new"],[name="filter_update"]').hide()
 			.filter(data.filter_configurable ? '[name="filter_update"]' : '[name="filter_new"]').show();
 
-		// // Template groups multiselect.
-		// $('#tpl_groupids_' + data.uniqid, container).multiSelectHelper({
-		// 	id: 'tpl_groupids_' + data.uniqid,
-		// 	object_name: 'hostGroup',
-		// 	name: 'tpl_groupids[]',
-		// 	data: data.filter_view_data.tpl_groups_multiselect || [],
-		// 	objectOptions: {
-		// 		enrich_parent_groups: 1
-		// 	},
-		// 	selectedLimit: 1,
-		// 	popup: {
-		// 		parameters: {
-		// 			srctbl: 'template_groups',
-		// 			srcfld1: 'groupid',
-		// 			dstfrm: 'zbx_filter',
-		// 			dstfld1: 'tpl_groupids_' + data.uniqid,
-		// 			with_templates: 1,
-		// 			editable: 1,
-		// 			enrich_parent_groups: 1
-		// 		}
-		// 	}
-		// });
+		// Template groups multiselect.
+		$('#tpl_groupids_' + data.uniqid, container).multiSelectHelper({
+			id: 'tpl_groupids_' + data.uniqid,
+			object_name: 'hostGroup',
+			name: 'tpl_groupids[]',
+			data: data.filter_view_data.tpl_groups_multiselect || [],
+			objectOptions: {
+				enrich_parent_groups: 1
+			},
+			selectedLimit: 1,
+			popup: {
+				parameters: {
+					srctbl: 'template_groups',
+					srcfld1: 'groupid',
+					dstfrm: 'zbx_filter',
+					dstfld1: 'tpl_groupids_' + data.uniqid,
+					with_templates: 1,
+					editable: 1,
+					enrich_parent_groups: 1
+				}
+			}
+		});
 
 		// Templates multiselect.
-		// $('#templateids_' + data.uniqid, container).multiSelectHelper({
-		// 	id: 'templateids_' + data.uniqid,
-		// 	object_name: 'templates',
-		// 	name: 'templateids[]',
-		// 	data: data.filter_view_data.templates_multiselect || [],
-		// 	selectedLimit: 1,
-		// 	popup: {
-		// 		filter_preselect: {
-		// 			id: 'tpl_groupids_' + data.uniqid,
-		// 			submit_as: 'templategroupid'
-		// 		},
-		// 		parameters: {
-		// 			srctbl: 'templates',
-		// 			srcfld1: 'hostid',
-		// 			dstfrm: 'zbx_filter',
-		// 			dstfld1: 'templateids_' + data.uniqid,
-		// 			multiselect: 1
-		// 		}
-		// 	}
-		// });
+		$('#templateids_' + data.uniqid, container).multiSelectHelper({
+			id: 'templateids_' + data.uniqid,
+			object_name: 'templates',
+			name: 'templateids[]',
+			data: data.filter_view_data.templates_multiselect || [],
+			selectedLimit: 1,
+			popup: {
+				filter_preselect: {
+					id: 'tpl_groupids_' + data.uniqid,
+					submit_as: 'templategroupid'
+				},
+				parameters: {
+					srctbl: 'templates',
+					srcfld1: 'hostid',
+					dstfrm: 'zbx_filter',
+					dstfld1: 'templateids_' + data.uniqid,
+					multiselect: 1
+				}
+			}
+		});
 
 		// Template triggers multiselect.
 		$('#tpl_triggerids_' + data.uniqid, container).multiSelectHelper({
