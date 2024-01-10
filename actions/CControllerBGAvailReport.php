8f1477955d29c6252ef7edfba7a359f9abf086e2
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-10 12:27:31 +0100

    Update CControllerBGAvailReport.php

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -197,7 +197,7 @@ abstract class CControllerBGAvailReport extends CController {
 		// } // end of if problems
 		// Now just prepare needed data.sorted by Number of status changes
 		CArrayHelper::sort($triggers, [
-			['field' => 'cnt_event', 'order' => ZBX_SORT_DOWN],
+			['field' => 'cnt_event', 'order' => ZBX_SORT_UP],
 			'host', 'description', 'priority'
 		]);
 		// Now just prepare needed data.
