81f4588a7d3f4c273ee45dbead5e081c546ffc0c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-15 12:35:14 +0100

    update

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -122,8 +122,8 @@ $template = (new CForm('get'))
 		(new CVar('filter_name', '#{filter_name}'))->removeId(),
 		(new CVar('filter_show_counter', '#{filter_show_counter}'))->removeId(),
 		(new CVar('filter_custom_time', '#{filter_custom_time}'))->removeId(),
-		(new CVar('from', '#{from}'))->removeId()
-		// (new CVar('to', '#{to}'))->removeId()
+		(new CVar('from', '#{from}'))->removeId(),
+		(new CVar('to', '#{to}'))->removeId()
 	]);
 
 if (array_key_exists('render_html', $data)) {
