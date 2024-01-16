21a3b2e5b6da813efe8b33e7b5bf5625508ffea3
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-16 16:44:30 +0100

    Update CControllerBGTabFilterProfileUpdate.php

--- actions/CControllerBGTabFilterProfileUpdate.php
+++ actions/CControllerBGTabFilterProfileUpdate.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /*
 ** Zabbix
-** Copyright (C) 2001-2024 Zabbix SIA
+** Copyright (C) 2001-2021 Zabbix SIA
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -18,104 +18,57 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
+namespace Modules\BGmotAR\Actions;
 
-class CControllerProfileUpdate extends CController {
+use CController;
+use CControllerHost;
+use CControllerProblem;
+use CControllerResponseData;
+use CSettingsHelper;
+use CTabFilterProfile;
+
+/**
+ * Controller to update tab filter. Handles following events:
+ * - select tab;
+ * - expand/collapse active tab;
+ * - update filter properties;
+ * - save tab order.
+ */
+class CControllerBGTabFilterProfileUpdate extends CController {
+
+	public static $namespaces = [
+		CControllerHost::FILTER_IDX => CControllerHost::FILTER_FIELDS_DEFAULT,
+		CControllerProblem::FILTER_IDX => CControllerProblem::FILTER_FIELDS_DEFAULT,
+		CControllerBGAvailReport::FILTER_IDX => CControllerBGAvailReport::FILTER_FIELDS_DEFAULT
+	];
+
+	protected function checkPermissions() {
+		return ($this->getUserType() >= USER_TYPE_ZABBIX_USER);
+	}
 
 	protected function checkInput() {
 		$fields = [
 			'idx' =>		'required|string',
 			'value_int' =>	'int32',
 			'value_str' =>	'string',
-			'idx2' =>		'array_id'
+			'idx2' =>		'id'
 		];
 
 		$ret = $this->validateInput($fields);
 
 		if ($ret) {
-			switch ($this->getInput('idx')) {
-				case 'web.action.list.filter.active':
-				case 'web.actionlog.filter.active':
-				case 'web.auditacts.filter.active':
-				case 'web.auditlog.filter.active':
-				case 'web.avail_report.filter.active':
-				case 'web.charts.filter.active':
-				case 'web.connector.filter.active':
-				case 'web.correlation.filter.active':
-				case 'web.dashboard.filter.active':
-				case 'web.dashboard.hostid':
-				case 'web.dashboard.last_widget_type':
-				case 'web.discovery.filter.active':
-				case 'web.discoveryconf.filter.active':
-				case 'web.hostgroups.filter.active':
-				case 'web.hostinventories.filter.active':
-				case 'web.hostinventoriesoverview.filter.active':
-				case 'web.hosts.filter.active':
-				case 'web.hosts.graphs.filter.active':
-				case 'web.hosts.host_discovery.filter.active':
-				case 'web.hosts.httpconf.filter.active':
-				case 'web.hosts.items.filter.active':
-				case 'web.hosts.triggers.filter.active':
-				case 'web.hostsmon.filter.active':
-				case 'web.httpdetails.filter.active':
-				case 'web.item.graph.filter.active':
-				case 'web.layout.mode':
-				case 'web.maintenance.filter.active':
-				case 'web.media_types.filter.active':
-				case 'web.modules.filter.active':
-				case 'web.problem.filter.active':
-				case 'web.proxies.filter.active':
-				case 'web.scheduledreport.filter.active':
-				case 'web.scripts.filter.active':
-				case 'web.search.hats.'.SECTION_SEARCH_HOSTS.'.state':
-				case 'web.search.hats.'.SECTION_SEARCH_TEMPLATES.'.state':
-				case 'web.search.hats.'.SECTION_SEARCH_HOSTGROUP.'.state':
-				case 'web.service.filter.active':
-				case 'web.service_actions.filter.active':
-				case 'web.sidebar.mode':
-				case 'web.sla.list.filter.active':
-				case 'web.slareport.list.filter.active':
-				case 'web.sysmapconf.filter.active':
-				case 'web.templategroups.filter.active':
-				case 'web.templates.filter.active':
-				case 'web.templates.graphs.filter.active':
-				case 'web.templates.host_discovery.filter.active':
-				case 'web.templates.httpconf.filter.active':
-				case 'web.templates.items.filter.active':
-				case 'web.templates.triggers.filter.active':
-				case 'web.token.filter.active':
-				case 'web.toptriggers.filter.active':
-				case 'web.tr_events.hats.'.SECTION_HAT_EVENTACTIONS.'.state':
-				case 'web.tr_events.hats.'.SECTION_HAT_EVENTLIST.'.state':
-				case 'web.user.filter.active':
-				case 'web.user.token.filter.active':
-				case 'web.usergroup.filter.active':
-				case 'web.web.filter.active':
-					$ret = true;
-					break;
+			$idx_cunks = explode('.', $this->getInput('idx'));
+			$property = array_pop($idx_cunks);
+			$idx = implode('.', $idx_cunks);
+			$supported = ['selected', 'expanded', 'properties', 'taborder'];
 
-				case 'web.dashboard.widget.geomap.default_view':
-				case 'web.dashboard.widget.geomap.severity_filter':
-				case (bool) preg_match('/web.dashboard.widget.navtree.item-\d+.toggle/', $this->getInput('idx')):
-				case 'web.dashboard.widget.navtree.item.selected':
-					$ret = $this->hasInput('idx2');
-					break;
+			$ret = (in_array($property, $supported) && array_key_exists($idx, static::$namespaces));
 
-				default:
-					$ret = false;
+			if ($property === 'selected' || $property === 'expanded') {
+				$ret = ($ret && $this->hasInput('value_int'));
 			}
-		}
-
-		if ($ret) {
-			switch ($this->getInput('idx')) {
-				case 'web.dashboard.last_widget_type':
-				case 'web.dashboard.widget.geomap.default_view':
-				case 'web.dashboard.widget.geomap.severity_filter':
-					$ret = $this->hasInput('value_str');
-					break;
-
-				default:
-					$ret = $this->hasInput('value_int');
-					break;
+			else if ($property === 'properties' || $property === 'taborder') {
+				$ret = ($ret && $this->hasInput('value_str'));
 			}
 		}
 
@@ -126,79 +79,98 @@ class CControllerProfileUpdate extends CController {
 		return $ret;
 	}
 
-	protected function checkPermissions() {
-		return ($this->getUserType() >= USER_TYPE_ZABBIX_USER);
+	protected function doAction() {
+		$data = $this->getInputAll() + [
+			'value_int' => 0,
+			'value_str' => '',
+			'idx2' => 0
+		];
+		$idx_cunks = explode('.', $this->getInput('idx'));
+		$property = array_pop($idx_cunks);
+		$idx = implode('.', $idx_cunks);
+		$defaults = static::$namespaces[$idx];
+
+		if (array_key_exists('from', $defaults) || array_key_exists('to', $defaults)) {
+			$defaults += [
+				'from' => 'now-4h',
+				'to' => 'now'
+			];
+		}
+
+		$filter = (new CTabFilterProfile($idx, $defaults))->read();
+
+		switch ($property) {
+			case 'selected':
+				$dynamictabs = count($filter->tabfilters);
+
+				if ($data['value_int'] >= 0 && $data['value_int'] < $dynamictabs) {
+					$filter->selected = (int) $data['value_int'];
+				}
+
+				break;
+
+			case 'properties':
+				$properties = [];
+				parse_str($this->getInput('value_str'), $properties);
+				$filter->setTabFilter($this->getInput('idx2'), $this->cleanProperties($properties));
+
+				break;
+
+			case 'taborder':
+				$filter->sort($this->getInput('value_str'));
+
+				break;
+
+			case 'expanded':
+				$filter->expanded = ($data['value_int'] > 0);
+
+				break;
+		}
+
+		$filter->update();
+
+		$data += [
+			'property' => $property,
+			'idx' => $idx
+		];
+		$response = new CControllerResponseData($data);
+		$this->setResponse($response);
 	}
 
-	protected function doAction() {
-		$idx = $this->getInput('idx');
+	/**
+	 * Clean fields data removing empty initial elements.
+	 *
+	 * @param array $properties  Array of submitted fields data.
+	 *
+	 * @return array
+	 */
+	protected function cleanProperties(array $properties): array {
+		if (array_key_exists('tags', $properties)) {
+			$tags = array_filter($properties['tags'], function ($tag) {
+				return !($tag['tag'] === '' && $tag['value'] === '');
+			});
 
-		DBstart();
-		switch ($idx) {
-			// PROFILE_TYPE_STR
-			case 'web.dashboard.last_widget_type':
-				$value_str = $this->getInput('value_str');
-				if ($value_str === '') {
-					CProfile::delete($idx);
-				}
-				else {
-					CProfile::update($idx, $value_str, PROFILE_TYPE_STR);
-				}
-				break;
-			case 'web.dashboard.widget.geomap.default_view':
-			case 'web.dashboard.widget.geomap.severity_filter':
-				$value_str = $this->getInput('value_str');
-				if ($value_str === '') { // default value
-					CProfile::delete($idx, $this->getInput('idx2'));
-				}
-				else {
-					foreach ($this->getInput('idx2') as $idx2) {
-						CProfile::update($idx, $value_str, PROFILE_TYPE_STR, $idx2);
-					}
-				}
-				break;
-
-			// PROFILE_TYPE_INT
-			case (bool) preg_match('/web.dashboard.widget.navtree.item-\d+.toggle/', $this->getInput('idx')):
-				$value_int = $this->getInput('value_int');
-				if ($value_int == 1) { // default value
-					CProfile::delete($idx, $this->getInput('idx2'));
-				}
-				else {
-					foreach ($this->getInput('idx2') as $idx2) {
-						CProfile::update($idx, $value_int, PROFILE_TYPE_INT, $idx2);
-					}
-				}
-				break;
-
-			case 'web.dashboard.widget.navtree.item.selected':
-				$value_int = $this->getInput('value_int');
-				foreach ($this->getInput('idx2') as $idx2) {
-					CProfile::update($idx, $value_int, PROFILE_TYPE_INT, $idx2);
-				}
-				break;
-
-			case 'web.layout.mode':
-				$value_int = $this->getInput('value_int');
-				CViewHelper::saveLayoutMode($value_int);
-				break;
-
-			case 'web.sidebar.mode':
-				$value_int = $this->getInput('value_int');
-				CViewHelper::saveSidebarMode($value_int);
-				break;
-
-			default:
-				$value_int = $this->getInput('value_int');
-				if ($value_int == 1) { // default value
-					CProfile::delete($idx);
-				}
-				else {
-					CProfile::update($idx, $value_int, PROFILE_TYPE_INT);
-				}
+			if ($tags) {
+				$properties['tags'] = array_values($tags);
+			}
+			else {
+				unset($properties['tags']);
+			}
 		}
-		DBend();
 
-		$this->setResponse(new CControllerResponseData(['main_block' => '']));
+		if (array_key_exists('inventory', $properties)) {
+			$inventory = array_filter($properties['inventory'], function ($field) {
+				return ($field['value'] !== '');
+			});
+
+			if ($inventory) {
+				$properties['inventory'] = array_values($inventory);
+			}
+			else {
+				unset($properties['inventory']);
+			}
+		}
+
+		return $properties;
 	}
 }
