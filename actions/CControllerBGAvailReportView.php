063c60042915b50230aee651a3bb1dd7ed946714
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-16 02:16:25 +0100

    Update CControllerBGAvailReportView.php

--- actions/CControllerBGAvailReportView.php
+++ actions/CControllerBGAvailReportView.php
@@ -70,7 +70,7 @@ class CControllerBGAvailReportView extends CControllerBGAvailReport {
 		// filter
 		$filter = $filter_tabs[$profile->selected];
 		$refresh_curl = (new CUrl('zabbix.php'));
-		$filter['action'] = 'availreport.view.update';
+		$filter['action'] = 'tabfilter.profile.update';
 		$filter['action_from_url'] = $this->getAction();
 		array_map([$refresh_curl, 'setArgument'], array_keys($filter), $filter);
 
