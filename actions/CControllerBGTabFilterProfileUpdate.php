1204800876efcf9b1581b1459738851620695117
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-16 17:22:26 +0100

    Update CControllerBGTabFilterProfileUpdate.php

--- actions/CControllerBGTabFilterProfileUpdate.php
+++ actions/CControllerBGTabFilterProfileUpdate.php
@@ -1,197 +1,113 @@
-<?php declare(strict_types=1);
-/*
-** Zabbix
-** Copyright (C) 2001-2021 Zabbix SIA
-**
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-**/
+<?php declare(strict_types = 1);
 
-namespace Modules\BGmotAR\Actions;
+namespace Modules\LMFR\Actions;
 
-use CController;
-use CControllerHost;
-use CControllerProblem;
+use CRoleHelper;
 use CControllerResponseData;
-use CSettingsHelper;
+use CControllerResponseFatal;
 use CTabFilterProfile;
+use CUrl;
+use CWebUser;
 
-/**
- * Controller to update tab filter. Handles following events:
- * - select tab;
- * - expand/collapse active tab;
- * - update filter properties;
- * - save tab order.
- */
-class CControllerBGTabFilterProfileUpdate extends CController {
+class CControllerBGAvailReportView extends CControllerBGAvailReport {
 
-	public static $namespaces = [
-		CControllerHost::FILTER_IDX => CControllerHost::FILTER_FIELDS_DEFAULT,
-		CControllerProblem::FILTER_IDX => CControllerProblem::FILTER_FIELDS_DEFAULT,
-		CControllerBGAvailReport::FILTER_IDX => CControllerBGAvailReport::FILTER_FIELDS_DEFAULT
-	];
-
-	protected function checkPermissions() {
-		return ($this->getUserType() >= USER_TYPE_ZABBIX_USER);
+	protected function init() {
+		$this->disableCsrfValidation();
 	}
 
 	protected function checkInput() {
+		error_reporting(0);
 		$fields = [
-			'idx' =>		'required|string',
-			'value_int' =>	'int32',
-			'value_str' =>	'string',
-			'idx2' =>		'id'
+			'name' =>			'string',
+			'mode' =>			'in '.AVAILABILITY_REPORT_BY_HOST.','.AVAILABILITY_REPORT_BY_TEMPLATE,
+			'tpl_groupids' =>		'array_id',
+			'templateids' =>		'array_id',
+			'tpl_triggerids' =>		'array_id',
+			'hostgroupids' =>		'array_id',
+			'hostids' =>			'array_id',
+			'triggerids'  =>		'array_id',
+			'filter_reset' =>		'in 1',
+			'only_with_problems' =>		'in 0,1',
+			'page' =>			'ge 1',
+			'counter_index' =>		'ge 0',
+			'from' =>			'range_time',
+			'to' =>				'range_time',
+			'sort' =>			'in name,status,cnt_event',
+			'sortorder' =>			'in '.ZBX_SORT_UP.','.ZBX_SORT_DOWN,
 		];
 
-		$ret = $this->validateInput($fields);
-
-		if ($ret) {
-			$idx_cunks = explode('.', $this->getInput('idx'));
-			$property = array_pop($idx_cunks);
-			$idx = implode('.', $idx_cunks);
-			$supported = ['selected', 'expanded', 'properties', 'taborder'];
-
-			$ret = (in_array($property, $supported) && array_key_exists($idx, static::$namespaces));
-
-			if ($property === 'selected' || $property === 'expanded') {
-				$ret = ($ret && $this->hasInput('value_int'));
-			}
-			else if ($property === 'properties' || $property === 'taborder') {
-				$ret = ($ret && $this->hasInput('value_str'));
-			}
-		}
+		$ret = $this->validateInput($fields) && $this->validateTimeSelectorPeriod();
 
 		if (!$ret) {
-			$this->setResponse(new CControllerResponseData(['main_block' => '']));
+			$this->setResponse(new CControllerResponseFatal());
 		}
 
 		return $ret;
 	}
 
+	protected function checkPermissions() {
+		return $this->checkAccess(CRoleHelper::UI_REPORTS_AVAILABILITY_REPORT);
+	}
+
 	protected function doAction() {
-		$data = $this->getInputAll() + [
-			'value_int' => 0,
-			'value_str' => '',
-			'idx2' => 0
-		];
-		$idx_cunks = explode('.', $this->getInput('idx'));
-		$property = array_pop($idx_cunks);
-		$idx = implode('.', $idx_cunks);
-		$defaults = static::$namespaces[$idx];
+		$filter_tabs = [];
 
-		if (array_key_exists('from', $defaults) || array_key_exists('to', $defaults)) {
-			$defaults += [
-				'from' => 'now-'.CSettingsHelper::get(CSettingsHelper::PERIOD_DEFAULT),
-				'to' => 'now'
-			];
+		$profile = (new CTabFilterProfile(static::FILTER_IDX, static::FILTER_FIELDS_DEFAULT))->read();
+		if ($this->hasInput('filter_reset')) {
+			$profile->reset();
+		}
+		else {
+			$profile->setInput($this->cleanInput($this->getInputAll()));
 		}
 
-		$filter = (new CTabFilterProfile($idx, $defaults))->read();
+		foreach ($profile->getTabsWithDefaults() as $index => $filter_tab) {
+			if ($index == $profile->selected) {
+				// Initialize multiselect data for filter_scr to allow tabfilter correctly handle unsaved state.
+				$filter_tab['filter_src']['filter_view_data'] = $this->getAdditionalData($filter_tab['filter_src']);
+			}
 
-		switch ($property) {
-			case 'selected':
-				$dynamictabs = count($filter->tabfilters);
-
-				if ($data['value_int'] >= 0 && $data['value_int'] < $dynamictabs) {
-					$filter->selected = (int) $data['value_int'];
-				}
-
-				break;
-
-			case 'properties':
-				$properties = [];
-				parse_str($this->getInput('value_str'), $properties);
-				$filter->setTabFilter($this->getInput('idx2'), $this->cleanProperties($properties));
-
-				break;
-
-			case 'taborder':
-				$filter->sort($this->getInput('value_str'));
-
-				break;
-
-			case 'expanded':
-				$filter->expanded = ($data['value_int'] > 0);
-
-				break;
+			$filter_tabs[] = $filter_tab + ['filter_view_data' => $this->getAdditionalData($filter_tab)];
 		}
 
-		$filter->update();
-
-		$data += [
-			'property' => $property,
-			'idx' => $idx
-		];
-
-
-		$filter = static::FILTER_FIELDS_DEFAULT;
-
-		$this->getInputs($filter, array_keys($filter));
-		$filter = $this->cleanInput($filter);
-		$prepared_data = $this->getData($filter);
-
-		$view_url = (new CUrl())
-			->setArgument('action', 'availreport.view')
-			->removeArgument('page');
+		// filter
+		$filter = $filter_tabs[$profile->selected];
+		$refresh_curl = (new CUrl('zabbix.php'));
+		$filter['action'] = 'availreport.view.refresh';
+		$filter['action_from_url'] = $this->getAction();
+		array_map([$refresh_curl, 'setArgument'], array_keys($filter), $filter);
 
 		$data = [
+			'action' => $this->getAction(),
+			'tabfilter_idx' => static::FILTER_IDX,
 			'filter' => $filter,
-			'view_curl' => $view_url
-		] + $prepared_data;
+			'filter_view' => 'reports.availreport.filter',
+			'filter_defaults' => $profile->filter_defaults,
+			'tabfilter_options' => [
+				'idx' => static::FILTER_IDX,
+				'selected' => $profile->selected,
+				'support_custom_time' => 1,
+				'expanded' => $profile->expanded,
+				'page' => $filter['page'],
+				'timeselector' => [
+					'from' => $profile->from,
+					'to' => $profile->to,
+					'disabled' => false
+				] + getTimeselectorActions($profile->from, $profile->to)
+			],
+			'filter_tabs' => $filter_tabs,
+			'refresh_url' => $refresh_curl->getUrl(),
+			'refresh_interval' => CWebUser::getRefresh() * 10000, //+++1000,
+			'page' => $this->getInput('page', 1)
+		] + $this->getData($filter);
 
 		$response = new CControllerResponseData($data);
+		$response->setTitle(_('Top Triggers Recurrence'));
+
+		if ($data['action'] === 'availreport.view.csv') {
+			$response->setFileName('zbx_availability_report_export.csv');
+		}
+
 		$this->setResponse($response);
-	// }
-
-	// 	$response = new CControllerResponseData($data);
-	// 	$this->setResponse($response);
-	}
-
-	/**
-	 * Clean fields data removing empty initial elements.
-	 *
-	 * @param array $properties  Array of submitted fields data.
-	 *
-	 * @return array
-	 */
-	protected function cleanProperties(array $properties): array {
-		if (array_key_exists('tags', $properties)) {
-			$tags = array_filter($properties['tags'], function ($tag) {
-				return !($tag['tag'] === '' && $tag['value'] === '');
-			});
-
-			if ($tags) {
-				$properties['tags'] = array_values($tags);
-			}
-			else {
-				unset($properties['tags']);
-			}
-		}
-
-		if (array_key_exists('inventory', $properties)) {
-			$inventory = array_filter($properties['inventory'], function ($field) {
-				return ($field['value'] !== '');
-			});
-
-			if ($inventory) {
-				$properties['inventory'] = array_values($inventory);
-			}
-			else {
-				unset($properties['inventory']);
-			}
-		}
-
-		return $properties;
 	}
 }
+?>
