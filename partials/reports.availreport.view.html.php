38c33b6930e9b19405b16c6a9b661373ac1c6e6e
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-24 18:17:07 +0100

    Update reports.availreport.view.html.php

--- partials/reports.availreport.view.html.php
+++ partials/reports.availreport.view.html.php
@@ -21,7 +21,7 @@ $allowed_ui_problems = CWebUser::checkAccess(CRoleHelper::UI_MONITORING_PROBLEMS
 
 $triggers = $data['triggers'];
 
-print_r($data['hosts']['status']);
+print_r($data['hosts']);
 // $tags = makeTags($triggers, true, 'triggerid', ZBX_TAG_COUNT_DEFAULT);
 // foreach ($triggers as &$trigger) {
 // 	$trigger['tags'] = $tags[$trigger['triggerid']];
