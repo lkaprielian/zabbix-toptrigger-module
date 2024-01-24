9caa04615ee06833c1304e0d603892618a643c91
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-24 17:55:17 +0100

    Update reports.availreport.view.html.php

--- partials/reports.availreport.view.html.php
+++ partials/reports.availreport.view.html.php
@@ -21,6 +21,7 @@ $allowed_ui_problems = CWebUser::checkAccess(CRoleHelper::UI_MONITORING_PROBLEMS
 
 $triggers = $data['triggers'];
 
+print_r($data['hosts']);
 // $tags = makeTags($triggers, true, 'triggerid', ZBX_TAG_COUNT_DEFAULT);
 // foreach ($triggers as &$trigger) {
 // 	$trigger['tags'] = $tags[$trigger['triggerid']];
@@ -34,9 +35,9 @@ foreach ($triggers as $trigger) {
 	
 
 	$hostName = (new CLinkAction($hostName))->setMenuPopup(CMenuPopupHelper::getHost($hostId));
-	if ($trigger['hosts'][0]['hostid']['status'] == HOST_STATUS_NOT_MONITORED) {
-		$hostName->addClass(ZBX_STYLE_RED);
-	}
+	// if ($data['hosts'][$hostId]['status'] == HOST_STATUS_NOT_MONITORED) {
+	// 	$hostName->addClass(ZBX_STYLE_RED);
+	// }
 
 	$triggerDescription = (new CLinkAction($trigger['description']))
 		->setMenuPopup(CMenuPopupHelper::getTrigger([
