2ca3bb558b46de6bde62c87cf2e024d55add2be8
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-29 12:00:35 +0100

    Update CControllerBGAvailReport.php

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -27,8 +27,8 @@ abstract class CControllerBGAvailReport extends CController {
 		'hostids' => [],
 		'only_with_problems' => 1,
         'page' => null,
-		'from' => '',
-		'to' => '',
+		'from' => 'now-1h',
+		'to' => 'now',
 		'sort' => 'name',
 		'sortorder' => ZBX_SORT_DOWN
 	];
