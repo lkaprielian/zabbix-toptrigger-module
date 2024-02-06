f226aeea4873a0de7f36a95250462b4a34ec4828
lkaprielian <lisakaprielian.lk@gmail.com>
2024-02-06 11:54:38 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -8,11 +8,10 @@ class Module extends \Zabbix\Core\CModule {
 	public function init(): void {
 		// Initialize main menu (CMenu class instance).
 		APP::Component()->get('menu.main')
-			->findOrAdd(_('Reports'))
-				->getSubmenu()
-					->insertAfter('Availability report', (new \CMenuItem(_('Top Triggers Recurrence')))
-						->setAction('availreport.view')
-					);
+			->add(new CMenuItem(_('Tools')))
+				->insertAfter('', (new \CMenuItem(_('Recurrence')))
+					->setAction('availreport.view')
+				);
 	}
 }
 ?>
