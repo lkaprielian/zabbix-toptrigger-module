232a7e3b52da8de94543120b7db5edb26ddd3a2e
lkaprielian <lisakaprielian.lk@gmail.com>
2024-02-06 11:59:50 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -4,14 +4,24 @@ namespace Modules\LMFR;
  
 use APP;
  
-class Module extends \Zabbix\Core\CModule {
-	public function init(): void {
-		// Initialize main menu (CMenu class instance).
-		APP::Component()->get('menu.main')
-			->add((new CMenuItem(_('Tools')))
-				// ->insertAfter('', (new \CMenuItem(_('Recurrence')))
-				->setAction('availreport.view')
-			);
-	}
+// class Module extends \Zabbix\Core\CModule {
+// 	public function init(): void {
+// 		// Initialize main menu (CMenu class instance).
+// 		APP::Component()->get('menu.main')
+// 			->findOrAdd(_('Reports'))
+// 				->getSubmenu()
+// 					->insertAfter('Availability report', (new \CMenuItem(_('Top Triggers Recurrence')))
+// 						->setAction('availreport.view')
+// 					);
+// 	}
+// }
+
+
+class Module extends CModule {
+    public function init(): void {
+        APP::Component()->get('menu.main')
+            ->add((new CMenuItem(_('Tools')))
+            ->setAction('availreport.view'));
+    }
 }
 ?>
