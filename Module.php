22b4dbc11d76f3da16a2dbf9718651ae881215c6
lkaprielian <lisakaprielian.lk@gmail.com>
2024-02-06 12:15:23 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -21,11 +21,15 @@ use Zabbix\Core\CModule,
 
 
 class Module extends \Zabbix\Core\CModule {
-    public function init(): void {
-        APP::Component()->get('menu.main')
-            ->add((new CMenuItem(_('Tools')))
-				->insertAfter('', (new \CMenuItem(_('Recurrence')))
-            	->setAction('availreport.view')));
-    }
+	public function init(): void {
+		// Initialize main menu (CMenu class instance).
+		APP::Component()->get('menu.main')
+			->findOrAdd(_('Tools'))
+				->getSubmenu()
+					->insertAfter('', (new \CMenuItem(_('Recurrence')))
+						->setAction('availreport.view')
+					);
+
+		}
 }
 ?>
