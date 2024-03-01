911a1f2d7eb25a644949f5a35ebc25d97d5b9f43
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-01 15:21:26 +0100

    Update CControllerBGAvailReportView.php

--- actions/CControllerBGAvailReportView.php
+++ actions/CControllerBGAvailReportView.php
@@ -33,6 +33,7 @@ class CControllerBGAvailReportView extends CControllerBGAvailReport {
 			'to' =>				'range_time',
 			'sort' =>			'in name,status,cnt_event',
 			'sortorder' =>			'in '.ZBX_SORT_UP.','.ZBX_SORT_DOWN,
+			'filter_name' =>			'string'
 		];
 
 		$ret = $this->validateInput($fields) && $this->validateTimeSelectorPeriod();
